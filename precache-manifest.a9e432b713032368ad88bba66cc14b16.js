06a508662e0ef3b39ce7714e656ab244dbdd7d34
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2020-05-03 21:39:03 +0400

    Updates

---
+++ precache-manifest.a9e432b713032368ad88bba66cc14b16.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d4e50c3c64939f184b9a96f96932d7",
+    "url": "/Bootcamp_M5/index.html"
+  },
+  {
+    "revision": "16fcae03fa5561e8195d",
+    "url": "/Bootcamp_M5/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "0ef6f52bdeabcba3f44f",
+    "url": "/Bootcamp_M5/static/css/main.1f5214cf.chunk.css"
+  },
+  {
+    "revision": "16fcae03fa5561e8195d",
+    "url": "/Bootcamp_M5/static/js/2.f84b81f2.chunk.js"
+  },
+  {
+    "revision": "1f343f3b00bd42841cd23cdb6e2b08ec",
+    "url": "/Bootcamp_M5/static/js/2.f84b81f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef6f52bdeabcba3f44f",
+    "url": "/Bootcamp_M5/static/js/main.7863e1b4.chunk.js"
+  },
+  {
+    "revision": "ecf1fe27c6238132c7ff",
+    "url": "/Bootcamp_M5/static/js/runtime-main.d7e4b722.js"
+  },
+  {
+    "revision": "619a61c4e03e68279fec6d33149036d2",
+    "url": "/Bootcamp_M5/static/media/Logo.619a61c4.svg"
+  },
+  {
+    "revision": "90fc1b8a070b518d74f79fb0ed04686d",
+    "url": "/Bootcamp_M5/static/media/arrow.90fc1b8a.svg"
+  },
+  {
+    "revision": "0710b8e20ec08eb454ca69991487633e",
+    "url": "/Bootcamp_M5/static/media/lens.0710b8e2.svg"
+  }
+]);
